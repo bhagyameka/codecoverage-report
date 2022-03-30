ed4354eea91341fce17da6265c06b2a1f48938d4
bhagyameka <bhagyakakumanu@gmail.com>
2022-03-31 00:20:17 +0530

    Update HelloTest.java
    uncommented lines13,14,22

--- src/test/java/com/choudhury/codecoverage/HelloTest.java
+++ src/test/java/com/choudhury/codecoverage/HelloTest.java
@@ -10,8 +10,8 @@ public class HelloTest {
 
         // If you want to understand how jacoco works, then run 'mvn clean install' from the command line so that this test will run
         // and you can inspect the surefire argline to understand how jacoco works (make to sure to umcomment 2 lines below to so tha you have time to inspect )
-        //System.out.println("Thread.sleep of 60 seconds added to give chance to launch 'jvisualvm' or any process explorer tool to inspect the full java argLine for SureFire ");
-        //Thread.sleep(60000);
+        System.out.println("Thread.sleep of 60 seconds added to give chance to launch 'jvisualvm' or any process explorer tool to inspect the full java argLine for SureFire ");
+        Thread.sleep(60000);
 
         Hello hello = new Hello();
         hello.sayHello("en");
@@ -19,6 +19,6 @@ public class HelloTest {
         // ensure that the code coverage check fails,
         // then uncomment the line below to make it pass.
 
-        //hello.sayHello("fr");
+        hello.sayHello("fr");
     }
 }
